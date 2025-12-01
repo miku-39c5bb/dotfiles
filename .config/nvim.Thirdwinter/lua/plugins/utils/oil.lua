e5e21d8ad0de3c4ce135b2c3426b15750871cbd8
miku-39c5bb <shalshyt@163.com>
2025-12-02 00:53:20 +0800

    nvim，oil文件管理器

---
+++ .config/nvim.Thirdwinter/lua/plugins/utils/oil.lua
@@ -0,0 +1,53 @@
+function _G.get_oil_winbar()
+  local dir = require('oil').get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ':~')
+  else
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
+local detail = false
+
+return {
+  'stevearc/oil.nvim',
+  lazy = false,
+  keys = {
+    { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
+  },
+  dependencies = { 'echasnovski/mini.icons' },
+  config = function()
+    require('oil').setup {
+      default_file_explorer = true,
+      keymaps = {
+        ['<C-h>'] = false,
+        ['<C-l>'] = false,
+        ['<C-k>'] = false,
+        ['<C-j>'] = false,
+        ['<C-r>'] = 'actions.refresh',
+        ['<leader>y'] = 'actions.yank_entry',
+        ['g.'] = false,
+        ['zh'] = 'actions.toggle_hidden',
+        ['\\'] = { 'actions.select', opts = { horizontal = true } },
+        ['|'] = { 'actions.select', opts = { vertical = true } },
+        ['-'] = 'actions.close',
+        ['<leader>e'] = 'actions.close',
+        ['<BS>'] = 'actions.parent',
+        ['gd'] = {
+          desc = 'Toggle file detail view',
+          callback = function()
+            detail = not detail
+            if detail then
+              require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+            else
+              require('oil').set_columns { 'icon' }
+            end
+          end,
+        },
+      },
+      win_options = {
+        winbar = '%!v:lua.get_oil_winbar()',
+      },
+    }
+  end,
+}
