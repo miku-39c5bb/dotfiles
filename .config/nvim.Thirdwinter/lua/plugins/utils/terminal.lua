c19f34b835d5b0b4881a0460229c5e26c62b6a0b
miku-39c5bb <shalshyt@163.com>
2025-12-23 00:57:22 +0800

    nvim terminal A-q to normal

--- .config/nvim.Thirdwinter/lua/plugins/utils/terminal.lua
+++ .config/nvim.Thirdwinter/lua/plugins/utils/terminal.lua
@@ -3,10 +3,10 @@
 --   cmd = { 'Fterm', 'FTerm', 'Sterm', 'STerm', 'Tterm', 'TTerm', 'Vterm', 'VTerm' },
 -- }
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'kj', [[<C-\><C-n>]], opts)
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<A-q>', [[<C-\><C-n>]], opts)
+  -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  -- vim.keymap.set('t', 'kj', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
@@ -15,23 +15,23 @@ function _G.set_terminal_keymaps()
 end
 
 return {
-  'akinsho/toggleterm.nvim', 
-  version = "*", 
-  opts = 
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts =
   {
     --[[ things you want to change go here]]
   },
   config = function()
-    require("toggleterm").setup{
+    require("toggleterm").setup {
       shade_terminals = false
     }
     vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-    vim.api.nvim_set_keymap("n", "<F9>", "<cmd>ToggleTerm<CR>", {noremap = true, silent = true})
-    vim.api.nvim_set_keymap("t", "<F9>", "<cmd>ToggleTerm<CR>", {noremap = true, silent = true})
-    vim.api.nvim_set_keymap("i", "<F9>", "<cmd>ToggleTerm<CR>", {noremap = true, silent = true})
-    vim.api.nvim_set_keymap("n", "<F10>", "<cmd>ToggleTermToggleAll<CR>", {noremap = true, silent = true})
-    vim.api.nvim_set_keymap("t", "<F10>", "<cmd>ToggleTermToggleAll<CR>", {noremap = true, silent = true})
-    vim.api.nvim_set_keymap("i", "<F10>", "<cmd>ToggleTermToggleAll<CR>", {noremap = true, silent = true})
-    vim.api.nvim_set_keymap("t", "<C-n>", "<cmd>ToggleTermSetName<CR>", {noremap = true, silent = true})
+    vim.api.nvim_set_keymap("n", "<F9>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<F9>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<F9>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<F10>", "<cmd>ToggleTermToggleAll<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<F10>", "<cmd>ToggleTermToggleAll<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<F10>", "<cmd>ToggleTermToggleAll<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<C-n>", "<cmd>ToggleTermSetName<CR>", { noremap = true, silent = true })
   end,
 }
